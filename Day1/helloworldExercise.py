7fb08ff18af6ee4354c4854fff8f73534d14d257
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-12-04 18:41:13 +0100

    ch: Day1 completed

---
+++ Day1/helloworldExercise.py
@@ -0,0 +1,55 @@
+# LEVEL 2
+# 1
+## To check the python version, do 'python --version' on the terminal
+
+# 2
+print(3 + 4)
+print(3 - 4)
+print(4 * 3)
+print(4 / 3)
+print(4 % 3)
+print(4 // 3)
+print(4 ** 3)
+print('\n')
+
+# 3
+print('Merit')
+print('Dike')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+print('\n')
+
+# 4
+print(type ('Merit'))
+print(type ('Dike'))
+print(type ('Nigeria'))
+print(type ('I am enjoying 30 days of python'))
+print(type (['Asabeneh', 'Python', 'Finland']))
+print(type (10))
+print(type (9.8))
+print(type (3.14))
+print(type (4 - 4j))
+print('\n')
+
+# LEVEL 3
+# 1
+print(type (25))
+print(type (26.8))
+print(type (6 - 9j))
+print(type ('Merit Dike'))
+print(type (True))
+print(type (False))
+print(type (['Merit', 27, True, 7+5j, {'isMaried': True}, {'January', 'March', 'June'}, (78, 90, 65)]))
+print(type ({'isMaried': True}))
+print(type ({'January', 'March', 'June'}))
+print(type ((57, 89, 70.6, 'Merit', True)))
+print('\n')
+
+# 2
+import math
+# one dimension
+print(math.sqrt((8 - 5) ** 2))
+print('\n')
+
+# two dimension
+print(math.sqrt(((10 - 2) ** 2) + (8 - 3) ** 2))
---
+++ Day1/practice.py
@@ -0,0 +1,44 @@
+# Single line comment
+    # This is the first comment
+    # This is the second comment
+    # Python is eating the world
+    
+# Multiple line comment
+"""This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world"""
+
+# string data type - If a string is more than one sentence then we use a triple quote.
+'Asabeneh'
+'Finland'
+'Python'
+'I love teaching'
+'I hope you are enjoying the first day of 30DaysOfPython Challenge'
+
+# boolean - F and T  is always in capital letter
+True  #  Is the light on? If it is on, then the value is True
+False # Is the light on? If it is off, then the value is False
+
+# lists (ordered collection of data)- just like arrays in jaascript
+[0, 1, 2, 3, 4, 5]  # all are the same data types - a list of numbers
+['Banana', 'Orange', 'Mango', 'Avocado'] # all the same data types - a list of strings (fruits)
+['Finland','Estonia', 'Sweden','Norway'] # all the same data types - a list of strings (countries)
+['Banana', 10, False, 9.81] # different data types in the list - string, integer, boolean and float
+
+# dictionary (unordered collection of data) - just like objects in javascript having key and value pairs
+{
+'first_name':'Asabeneh',
+'last_name':'Yetayeh',
+'country':'Finland', 
+'age':250, 
+'is_married':True,
+'skills':['JS', 'React', 'Node', 'Python']
+}
+
+# Tuple - just like lists but connot be modified after creation (immutable)
+('Asabeneh', 'Pawel', 'Brook', 'Abraham', 'Lidiya') # Names
+('Earth', 'Jupiter', 'Neptune', 'Mars', 'Venus', 'Saturn', 'Uranus', 'Mercury') # planets
+
+# Sets - just like javascript set stores unique data
+{2, 4, 3, 5}
+{3.14, 9.81, 2.7} # order is not important in set
